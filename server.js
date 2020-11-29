ba14ee4060de8d3499fa3031c2adaefbdcb30fe0
RameshMF <ramesh24fadatare@gmail.com>
2020-11-29 12:17:17 +0530

    deploy angular app to heroku demo code

--- server.js
+++ server.js
@@ -5,12 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static('./dist/angular-app-heroku'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
-});
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/angular-app-heroku/'}),
+);
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
